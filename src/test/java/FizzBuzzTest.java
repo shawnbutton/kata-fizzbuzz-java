711e6951b5aeb7bfcf70deefcb8c02d6b9e0f84d
shawn <shawnbutton@gmail.com>
2022-07-07 11:55:23 -0400

    create fresh kata project

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testThisWorks() {
+        Assertions.fail();
+    }
+}
